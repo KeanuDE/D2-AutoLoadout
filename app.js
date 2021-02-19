55b49fa7a0ee20356402c253beb3b5b4b1a9275e
KeanuLox <skyloxkeanu@gmail.com>
2021-02-19 14:09:51 +0100

    .

--- app.js
+++ app.js
@@ -8,22 +8,16 @@ function getQueries() {
     location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
     return queryDict;
 }
-
+let apiKey = "5c48d29c9ee04b5e8f4bb13377fa8101";
 async function getToken() {
-    const response = await fetch("https://www.bungie.net/platform/app/oauth/token/", {
-        method: 'POST',
-        mode: 'no-cors',
-        cache: 'no-cache',
+    $.ajax({
+        url: "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'X-API-Key': "5c48d29c9ee04b5e8f4bb13377fa8101"
-        },
-        body: new URLSearchParams({
-            'grant_type': 'authorization_code',
-            'code': String(getQueries().code)
-        })
+         "X-API-Key": apiKey
+        }
+    }).done(function(json){
+        return json.Response.data.inventoryItem.itemName; //Gjallarhorn
     });
-    return response;
 }
 
 if(getQueries().code == undefined) {
