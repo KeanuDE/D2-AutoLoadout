c9e6c1c9e928b545b34b791d26558aa69b0af558
KeanuLox <skyloxkeanu@gmail.com>
2021-02-19 09:29:53 +0100

    first testing

--- app.js
+++ app.js
@@ -3,3 +3,28 @@
 *   link: https://www.bungie.net/en/oauth/authorize?response_type=code&client_id=35548
 */
 
+function getQueries() {
+    var queryDict = {}
+    location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
+    return queryDict;
+}
+
+async function getToken() {
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-API-Key': "5c48d29c9ee04b5e8f4bb13377fa8101"
+        },
+        body: "grant_type=authorization_code&code=" + getQueries().code
+    });
+    return response;
+}
+
+if(getQueries().code == undefined) {
+    $("login").css('display',block);
+} else {
+    $("login").css('display',block);
+}
